42be17534d52c42eac3a0d9bb343e5f2fd2c26a6
be-better-be-stronger <dangquocthanhsaclo@gmail.com>
2026-01-31 13:14:57 +0700

    Refactor app bootstrap and routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,26 @@
 import { Routes } from '@angular/router';
 import { AdminProductFormComponent } from './features/admin/components/product-form/product-form.component';
-import { LoginComponent } from './core/auth/login.component/login.component';
+import { LoginComponent } from './features/auth/login.component/login.component';
 import { adminGuard } from './core/guards/admin.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+   {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home')
+        .then(m => m.HomeComponent)
+  },
+
 
   // demo 403
-  { path: '403', loadComponent: () => import('./shares/pages/forbidden.component').then(m => m.ForbiddenComponent) },
+  { path: '403', loadComponent: () => import('./shared/pages/forbidden.component').then(m => m.ForbiddenComponent) },
 
   // admin
   { path: 'admin/products/new', component: AdminProductFormComponent, canActivate: [adminGuard] },
 
-  { path: '', redirectTo: 'admin/products/new', pathMatch: 'full' },
+  { path: 'admin', redirectTo: 'admin/products/new', pathMatch: 'full' },
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
 ];
