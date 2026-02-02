51673f65f85f3dc0b3fe34bfa4f730cd775c4682
be-better-be-stronger <dangquocthanhsaclo@gmail.com>
2026-02-02 14:22:06 +0700

    feat(admin-product): update admin product service

--- src/app/features/admin/services/admin-product.service.ts
+++ src/app/features/admin/services/admin-product.service.ts
@@ -3,7 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { ApiResponse } from '../../../core/models/api-response';
-import { ProductResponse, UpsertProductRequest } from '../../../core/models/product';
+import { PageProductRequest, ProductResponse, ProductStatusFilter, UpsertProductRequest } from '../../../core/models/product';
+import { Page } from '../../../core/models/page.model';
 
 @Injectable({ providedIn: 'root' })
 export class AdminProductService {
@@ -25,7 +26,13 @@ export class AdminProductService {
 
   disable(id: number): Observable<void> {
     return this.http
-      .patch<ApiResponse<void>>(`${this.api}/${id}`, {})
+      .patch<ApiResponse<void>>(`${this.api}/${id}/disable`, {})
+      .pipe(map(() => undefined));
+  }
+
+  enable(id: number): Observable<void> {
+    return this.http
+      .patch<ApiResponse<void>>(`${this.api}/${id}/enable`, {})
       .pipe(map(() => undefined));
   }
 
@@ -37,4 +44,43 @@ export class AdminProductService {
       .post<ApiResponse<string>>(`${this.api}/upload-image`, formData)
       .pipe(map(r => r.data));
   }
+
+  list(
+    req: PageProductRequest,
+    status?: ProductStatusFilter
+  ): Observable<Page<ProductResponse>> {
+
+    const statusParam =
+      status === 'active' ? true :
+        status === 'disabled' ? false :
+          undefined;
+
+    const q = req.q?.trim();
+    const sort = req.sort ?? undefined;
+    const dir = req.dir ?? undefined;
+    const cat = req.cat ?? undefined;
+
+    const params: Record<string, any> = {
+      page: req.page,
+      size: req.size,
+      ...(q ? { q } : {}),
+      ...(cat != null ? { cat } : {}),
+      ...(sort ? { sort } : {}),
+      ...(dir ? { dir } : {}),
+      ...(statusParam !== undefined ? { status: String(statusParam) } : {}),
+    };
+
+    return this.http.get<any>(this.api, { params: params as any }).pipe(
+      map(res => res.data as Page<ProductResponse>)
+    );
+  }
+
+
+
+
+
+
+
+
+
 }
